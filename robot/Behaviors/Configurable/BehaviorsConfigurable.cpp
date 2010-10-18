2f212355d4255569ec06d077d112354914aa53d4
Ryan Cahoon <rcahoon@andrew.cmu.edu>
2010-10-18 17:43:32 -0400

    Restoring BehaviorConfigurable.cpp after merge

--- robot/Behaviors/Configurable/BehaviorsConfigurable.cpp
+++ robot/Behaviors/Configurable/BehaviorsConfigurable.cpp
@@ -1,6 +1,8 @@
 #include "BehaviorsConfigurable.h"
 #include "Behaviors/RemoteControl/RemoteControl.h"
 #include "Behaviors/Null/NullBehaviors.h"
+#include "Behaviors/MoveToBall/MoveToBall.h"
+#include "Behaviors/ServoToBall/ServoToBall.h"
 #include "Behaviors/Test/BehaviorsTest.h"
 #include "shared/ConfigFile/ConfigFile.h"
 
@@ -21,9 +23,21 @@ BehaviorsConfigurable::BehaviorsConfigurable(ConfigFile & configFile,
     behaviors = new NullBehaviors();
   }
   // Should we use TestBehaviors?
+<<<<<<< HEAD
+  if (name.compare("Test") == 0) {
+    behaviors = new RoboCup2010::BehaviorsTest(configFile,_log,field);
+  }
+  else if (name.compare("ServoToBall") == 0) {
+    behaviors = new RoboCup2010::ServoToBall(configFile,_log);
+  }
+  else if (name.compare("MoveToBall") == 0) {
+    behaviors = new RoboCup2010::MoveToBall(configFile,_log);
+  }
+=======
   else if (name.compare("Test") == 0) {
     behaviors = new RoboCup2010::BehaviorsTest(configFile,_log,field);
   }
+>>>>>>> 87e45019bb7721a6b4cf21672ed1d5200c75d8a2
   else {
     LOG_WARN("Configurable Behaviors was not defined.");
   }
